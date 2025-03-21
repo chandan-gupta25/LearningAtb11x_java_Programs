1688d664180a3e2094471da611bffd6fe6c497db
Your Name <chandankg25@gmail.com>
2025-03-21 21:51:44 +0530

    21-march boolean

---
+++ src/ex_04_Operators/Lab04_boolean.java
@@ -0,0 +1,10 @@
+package ex_04_Operators;
+
+public class Lab04_boolean {
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 10;
+        boolean result = a > b;
+        System.out.println(result);
+    }
+}
---
+++ src/ex_04_Operators/Lab05_booleanage.java
@@ -0,0 +1,12 @@
+package ex_04_Operators;
+
+public class Lab05_booleanage {
+    public static void main(String[] args) {
+
+        int age_Aman = 15;
+        int age_Chandan = 30;
+        boolean result = age_Chandan >= age_Aman;
+        System.out.println(result);
+
+    }
+}
