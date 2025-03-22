bbc24d8e3775d84c6a45c7dd51173476b48e4b78
Your Name <chandankg25@gmail.com>
2025-03-22 08:42:28 +0530

    12th march task

---
+++ src/ex_04_Operators/Lab06_task12thmarch.java
@@ -0,0 +1,24 @@
+package ex_04_Operators;
+
+public class Lab06_task12thmarch {
+    public static void main(String[] args) {
+        //Practice all the exercises.
+        //
+        // X = 10, Y = 10, Z = 10 , Result
+        //
+        //Math.functions - Research about th3
+        //
+        //(x
+        //2
+        // +y
+        //2
+        // −∣z∣)
+
+        int x = 10;
+        int y = 10;
+        int z = 10;
+        double result = Math.cbrt(Math.pow(x, 2) + Math.pow(y, 2) - Math.abs(z));
+        System.out.println(result);
+        }
+
+}
