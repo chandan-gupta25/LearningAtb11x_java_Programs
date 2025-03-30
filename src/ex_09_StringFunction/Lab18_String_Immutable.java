82b747c7a812842c93d86f41d081d3dc9366d200
Your Name <chandankg25@gmail.com>
2025-03-30 16:55:07 +0530

    24-march-25 string function

---
+++ src/ex_09_StringFunction/Lab18_String_Immutable.java
@@ -0,0 +1,13 @@
+package ex_09_StringFunction;
+
+public class Lab18_String_Immutable {
+
+    public static void main(String[] args) {
+        String name = "Chandan";
+        name =name.toUpperCase(); // PRAMOD
+        System.out.println(name);
+        System.out.println(name.toLowerCase());
+
+
+    }
+}
---
+++ src/ex_09_StringFunction/Lab19_string_function.java
@@ -0,0 +1,14 @@
+package ex_09_StringFunction;
+
+public class Lab19_string_function {
+    public static void main(String[] args) {
+
+        char c = 'A';
+        System.out.println(c);
+
+        String random = "AbCd";
+        System.out.println(random.toUpperCase());
+        System.out.println(random.toLowerCase());
+        System.out.println(random.concat("E"));
+    }
+}
