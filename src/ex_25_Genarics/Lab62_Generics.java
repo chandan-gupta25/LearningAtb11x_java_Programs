a831bed06a7217701d2c03ff366d64ed78f14401
Your Name <chandankg25@gmail.com>
2025-04-13 23:55:26 +0530

    13_April_2025_generics

---
+++ src/ex_25_Genarics/Lab62_Generics.java
@@ -0,0 +1,15 @@
+package ex_25_Genarics;
+
+public class Lab62_Generics {
+    public static void main(String[] args) {
+
+        input_user(10, 20);
+        input_user("chandan", "gupta");
+
+    }
+
+    static <T> T input_user(T a, T b){
+        return null;
+    }
+
+}
---
+++ src/ex_25_Genarics/Lab63_Generics_class.java
@@ -0,0 +1,23 @@
+package ex_25_Genarics;
+
+public class Lab63_Generics_class {
+    public static void main(String[] args) {
+
+        GenericClass<Integer> objectRefer = new GenericClass(10);
+       // GenericClass<String> stringObjectRefer = new GenericClass("chandan");   if we pass string then T become String
+
+    }
+}  // this is a generics class
+class GenericClass <T>{       // if we pass integer then T become Integer
+    private T data;    // T is a place holder it can be any data type
+
+    public GenericClass(T data) {
+        this.data = data;
+    }
+
+    public T getData(){
+        return this.data;
+    }
+}
+
+
