1a56e5d8237a6c1e8f92dceeda088796c736928c
Your Name <chandankg25@gmail.com>
2025-04-14 00:37:53 +0530

    13_April_Collection_Frameworks/list

---
+++ src/List/Lab64_ArrayList.java
@@ -0,0 +1,20 @@
+package List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lab64_ArrayList {
+    public static void main(String[] args) {
+
+        //ArrayList arrayList = new ArrayList();
+        List arrayList = new ArrayList();
+        arrayList.add("chandan");
+        arrayList.add(12);
+        arrayList.add("gupta");
+        System.out.println(arrayList.isEmpty());
+        System.out.println(arrayList);
+        System.out.println(arrayList.size());
+
+    }
+
+}
